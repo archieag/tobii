d50dd18180ac27ddfaa2ab2ef650fedec52f3bdf
Archie <agrawal.archie@gmail.com>
2015-04-12 16:17:40 -0400

    Tobii

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActivatableElements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Program.EyeXHost.Connect(_behaviorMap1);
+            behaviorMap1.Add(_button1, new EyeXFramework.ActivatableBehavior(OnButton1Activated));
+        }
+            // Handle the activated event from the EyeX Engine to trigger a click
+        private void OnButton1Activated(object sender, EventArgs e)
+        {
+            _button1.PerformClick();
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ActivatableElements
+{
+    static class Program
+    {
+        private static EyeXFramework.Forms.FormsEyeXHost _eyeXHost = new EyeXFramework.Forms.FormsEyeXHost();
+        /// <summary>
+        /// Gets the singleton EyeX host instance.
+        /// </summary>
+        public static EyeXFramework.Forms.FormsEyeXHost EyeXHost
+        {
+
+            get { return _eyeXHost; }
+        }
+        [STAThread]
+        static void Main()
+        {
+            _eyeXHost.Start();
+            
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            
+            _eyeXHost.Dispose(); // always dispose on exit
+        }
+    }
+}
